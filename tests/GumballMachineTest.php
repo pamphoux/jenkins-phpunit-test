394a16834f91e7097a6501174dbb7c6ad023122f
pamphoux <pierre.amphoux@orange.fr>
2022-07-27 01:27:58 -0400

    first commit

---
+++ tests/GumballMachineTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require 'GumballMachine.php';
+
+class GumballMachineTest extends TestCase
+{
+    public $gumballMachineInstance;
+
+    public function setUp(): void
+    {
+        $this->gumballMachineInstance = new GumballMachine();
+    }
+
+    public function testIfWheelWorks(){
+    	// Suppose we have 100 gumballs...
+    	$this->gumballMachineInstance->setGumballs(100);
+
+    	// ... And we turn the wheel once...
+    	$this->gumballMachineInstance->turnWheel();
+
+    	// ... we should now have 99 gumballs remaining in the machine right?
+    	$this->assertEquals(99, $this->gumballMachineInstance->getGumballs()); 
+    }
+}
